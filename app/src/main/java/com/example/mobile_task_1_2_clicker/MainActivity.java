d90914fb9f9a44c14897cad223c33ffc3f04f1f4
goncharovkonstantin <goncharovkostya.1997@gmail.com>
2019-04-04 11:33:03 +0300

    Done

--- app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
+++ app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
@@ -4,13 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextClock;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mainText;
-    Button mainBtn;
+    ImageButton btnIncrease;
+    Button btnDecrease;
+    Button btnNullify;
 
     private long score = 0;
 
@@ -20,20 +23,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mainText = (TextView) findViewById(R.id.mainText);
-        mainBtn = (Button) findViewById(R.id.button);
+        btnIncrease = (ImageButton) findViewById(R.id.btnIncrease);
+        btnDecrease = (Button) findViewById(R.id.btnDecrease);
+        btnNullify = (Button) findViewById(R.id.btNullify);
 
-
-        View.OnClickListener clickListener = new View.OnClickListener() {
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                score ++;
-                String s = "Кликов: " + score;
-                mainText.setText(s.toCharArray(),0, s.length());
+                score += 1;
+                updateTextView();
             }
-        };
+        });
 
-        mainBtn.setOnClickListener(clickListener);
+        btnDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score = score == 0 ? 0 : score - 1;
+                updateTextView();
+            }
+        });
 
+        btnNullify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score = 0;
+                updateTextView();
+            }
+        });
+    }
+
+    private void updateTextView()
+    {
+        mainText.setText(getString(R.string.clicked) + " " + score);
     }
 }
 
